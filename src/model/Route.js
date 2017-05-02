52c5a28e70e98fb1d48e2990386289689de301dc
Yann Deshayes <deshayes.yann@gmail.com>
2017-05-02 16:45:38 +0200

    Fix field types and get function in Route

--- src/model/Route.js
+++ src/model/Route.js
@@ -1,15 +1,17 @@
 import Ressource from './Ressource';
 
 const paramDefaults = {};
+const creatableAndModifiableField = ['route', 'to', 'type', 'upstream', 'cache'];
 
 export default class Route extends Ressource {
   constructor(route, url) {
-    super(url, paramDefaults, { }, route);
+    super(url, paramDefaults, { }, route, creatableAndModifiableField, creatableAndModifiableField);
+    this._queryUrl = Ressource.getQueryUrl(url);
     this.id = '';
     this.project = '';
     this.environment = '';
-    this.route = '';
-    this.cache = [];
+    this.route = {};
+    this.cache = {};
     this.ssi = [];
     this.upstream = '';
     this.to = '';
@@ -19,7 +21,7 @@ export default class Route extends Ressource {
   static get(params, url) {
     const { id, ...queryParams } = params;
 
-    return super.get(`${url}/:id`, { name }, paramDefaults, queryParams);
+    return super.get(`${url}/:id`, { id }, paramDefaults, queryParams);
   }
 
   static query(params, url) {
