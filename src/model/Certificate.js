cbd4f686928dbdc68c806017fc3406c46c871def
Yann Deshayes <deshayes.yann@gmail.com>
2017-05-16 09:00:15 +0200

    Add certificate fields

--- src/model/Certificate.js
+++ src/model/Certificate.js
@@ -6,8 +6,14 @@ export default class Certificate extends Ressource {
   constructor(certificate, url) {
     super(url, paramDefaults, { }, certificate, ['key', 'certificate', 'chain']);
     this.key = '';
+    this.id = '';
     this.certificate = '';
     this.chain = [];
+    this.domains = [];
+    this.expires_at = '';
+    this.updated_at = '';
+    this.is_provisioned = true;
+    this.issuer = [];
     this._required = ['key', 'certificate'];
   }
 
