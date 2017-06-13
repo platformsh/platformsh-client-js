f4ecea2afc6b674aced3d2c1b797740f2bbaa54f
Yann Deshayes <deshayes.yann@gmail.com>
2017-06-13 08:54:43 +0200

    Introduce disk utility metrics helper

--- src/model/Metric.js
+++ src/model/Metric.js
@@ -2,6 +2,12 @@ import Ressource from './Ressource';
 
 const paramDefaults = {};
 
+const diskMetricRequest = (fromHour = 1) => `SELECT
+  MEAN("available:bytes") as available,
+  MEAN("total:bytes") as total
+  FROM disk WHERE time > now() - ${fromHour}h
+  GROUP BY project,instance,volume,time(5m)`;
+
 export default class Metrics extends Ressource {
   constructor(metric, url) {
     super(url, paramDefaults, {}, metric);
@@ -10,5 +16,11 @@ export default class Metrics extends Ressource {
 
   static get(params, url) {
     return super.get(url, {}, paramDefaults, params);
+  }
+
+  static getDiskMetrics(params, url) {
+    return super.get(url, {}, paramDefaults, {
+      q: diskMetricRequest(params.fromHour)
+    });
   }
 }
