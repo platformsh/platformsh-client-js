4cb98c4f7edcf7b8400a9eab590adc8fec3d635e
Patrik Karisch <p.karisch@pixelart.at>
2017-12-06 10:54:30 +0100

    Generate two builds, one for the browser, one for node

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var path = require('path');
 var env = require('yargs').argv.env;
+var merge = require('webpack-merge');
 
 var libraryName = 'platform-api';
 
@@ -27,11 +28,8 @@ var config = {
     'authentication/index':  __dirname + '/src/authentication',
   },
   output: {
-    path: __dirname + '/lib',
     filename: '[name].js',
     library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
   },
   module: {
     rules: [
@@ -52,4 +50,21 @@ var config = {
   plugins: plugins
 };
 
-module.exports = config;
+var client = merge(config, {
+  target: 'web',
+  output: {
+    path: __dirname + '/lib/client',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+});
+
+var server = merge(config, {
+  target: 'node',
+  output: {
+    path: __dirname + '/lib/server',
+    libraryTarget: 'commonjs',
+  },
+});
+
+module.exports = [client, server];
