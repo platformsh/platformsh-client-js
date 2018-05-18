3a97ed3faf5a4b91295c66485c69605afe73ee91
Yann Deshayes <deshayes.yann@gmail.com>
2018-05-18 08:20:57 +0200

    Introduce order model

---
+++ src/model/Order.js
@@ -0,0 +1,50 @@
+import Ressource from "./Ressource";
+import { getConfig } from "../config";
+
+const url = "/platform/orders/:id";
+const paramDefaults = {};
+
+export default class Order extends Ressource {
+  constructor(account) {
+    const { id } = account;
+    const { account_url } = getConfig();
+
+    super(`${account_url}${url}`, paramDefaults, { id }, account);
+    this.id = "";
+    this.status = "";
+    this.owner = "";
+    this.address = {};
+    this.vat_number = 0;
+    this.billing_period_start = "";
+    this.billing_period_end = "";
+    this.total = 0;
+    this.components = {};
+    this.currency = "";
+    this.invoice_url = "";
+    this.line_items = [];
+  }
+
+  static get(params, customUrl) {
+    const { id, ...queryParams } = params;
+    const { account_url } = getConfig();
+
+    return super.get(
+      customUrl || `${account_url}${url}`,
+      { id },
+      paramDefaults,
+      queryParams
+    );
+  }
+
+  static query(params) {
+    const { account_url } = getConfig();
+
+    return super.query(
+      this.getQueryUrl(`${account_url}${url}`),
+      {},
+      paramDefaults,
+      params,
+      data => data.commerce_order
+    );
+  }
+}
