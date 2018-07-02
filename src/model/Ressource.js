db179631e55303d4f9c2a278c3ef08abcfa37dc3
Yann Deshayes <deshayes.yann@gmail.com>
2018-07-02 14:56:45 +0200

    Calling update with a link should not check the #edit link

--- src/model/Ressource.js
+++ src/model/Ressource.js
@@ -1,20 +1,25 @@
-import pick from 'object.pick';
-import parse_url from 'parse_url';
+import pick from "object.pick";
+import parse_url from "parse_url";
 
-import _urlParser from '../urlParser';
-import request from '../api';
-import Result from './Result';
+import _urlParser from "../urlParser";
+import request from "../api";
+import Result from "./Result";
 
 const handler = {
   get(target, key) {
-    if((typeof key !== 'symbol' && !key.startsWith('_')) && key !== 'data' && target.hasOwnProperty(key)) {
+    if (
+      typeof key !== "symbol" &&
+      !key.startsWith("_") &&
+      key !== "data" &&
+      target.hasOwnProperty(key)
+    ) {
       return target.data && target.data[key];
     }
 
     return target[key];
   },
   set(target, key, value) {
-    if(key !== 'data' && target.hasOwnProperty(key)) {
+    if (key !== "data" && target.hasOwnProperty(key)) {
       target.data[key] = value;
       return true;
     }
@@ -25,7 +30,14 @@ const handler = {
 };
 
 export default class Ressource {
-  constructor(_url, paramDefaults, params, data = {}, _creatableField = [], _modifiableField = []) {
+  constructor(
+    _url,
+    paramDefaults,
+    params,
+    data = {},
+    _creatableField = [],
+    _modifiableField = []
+  ) {
     // This is an abstract class
     if (this.constructor === Ressource) {
       throw new Error("Can't instantiate abstract class");
@@ -33,13 +45,15 @@ export default class Ressource {
 
     this.copy(data);
 
-    const url = _url || this.getLink('self');
+    const url = _url || this.getLink("self");
 
     this._url = _urlParser(url, params, paramDefaults);
     const parsedUrl = parse_url(url);
 
-    if(parsedUrl[1] === 'http' || parsedUrl[1] === 'https') {
-      this._baseUrl = `${parsedUrl[1]}:${parsedUrl[2]}${parsedUrl[3]}${parsedUrl[4] ? `:${parsedUrl[4]}` : ''}`;
+    if (parsedUrl[1] === "http" || parsedUrl[1] === "https") {
+      this._baseUrl = `${parsedUrl[1]}:${parsedUrl[2]}${parsedUrl[3]}${
+        parsedUrl[4] ? `:${parsedUrl[4]}` : ""
+      }`;
     }
     this._creatableField = _creatableField;
     this._modifiableField = _modifiableField;
@@ -48,29 +62,37 @@ export default class Ressource {
     return new Proxy(this, handler);
   }
 
-  static getQueryUrl(_url = '') {
-    return _url.substring(0, _url.lastIndexOf('/'));
+  static getQueryUrl(_url = "") {
+    return _url.substring(0, _url.lastIndexOf("/"));
   }
 
   static get(_url, params, paramDefaults, queryParams) {
     const parsedUrl = _urlParser(_url, params, paramDefaults);
 
-    return request(parsedUrl, 'GET', queryParams).then(data => {
+    return request(parsedUrl, "GET", queryParams).then(data => {
       return new this.prototype.constructor(data, _url);
     });
   }
 
-  static query(_url, params, paramDefaults, queryParams, transformResultBeforeMap) {
+  static query(
+    _url,
+    params,
+    paramDefaults,
+    queryParams,
+    transformResultBeforeMap
+  ) {
     const parsedUrl = _urlParser(_url, params, paramDefaults);
 
-    return request(parsedUrl, 'GET', queryParams).then(data => {
+    return request(parsedUrl, "GET", queryParams).then(data => {
       let dataToMap = data;
 
-      if(transformResultBeforeMap) {
+      if (transformResultBeforeMap) {
         dataToMap = transformResultBeforeMap(data);
       }
 
-      return dataToMap.map(d => new this.prototype.constructor(d, `${_url}/${d.id}`));
+      return dataToMap.map(
+        d => new this.prototype.constructor(d, `${_url}/${d.id}`)
+      );
     });
   }
 
@@ -79,108 +101,119 @@ export default class Ressource {
   }
 
   /**
-  * Validate values for update.
-  *
-  * @param array values
-  *
-  * @return string{} An object of validation errors.
-  */
+   * Validate values for update.
+   *
+   * @param array values
+   *
+   * @return string{} An object of validation errors.
+   */
   checkUpdate(values = {}) {
     let errors;
 
-    for(let key in Object.keys(values)) {
-      errors = { ...errors, ...this.checkProperty(key, values[key])};
+    for (let key in Object.keys(values)) {
+      errors = { ...errors, ...this.checkProperty(key, values[key]) };
     }
     return Object.keys(errors).length ? errors : undefined;
   }
 
   update(data, _url) {
-    if(!this._modifiableField.length) {
+    if (!this._modifiableField.length) {
       throw new Error("Can't call update on this ressource");
     }
 
     const errors = this.checkUpdate(this.data);
 
-    if(errors) {
+    if (errors) {
       return Promise.reject(errors);
     }
+    let updateLink;
 
-    const updateLink = this.getLink('#edit');
-
-    if(!updateLink) {
-      throw new Error('Not allowed to edit');
+    try {
+      updateLink = this.getLink("#edit");
+    } catch (err) {
+      if (!_url) {
+        throw new Error("Not allowed to edit", err);
+      }
     }
 
-    const parsedUrl = _urlParser(_url || updateLink, this.data, this._paramDefaults);
+    const parsedUrl = _urlParser(
+      _url || updateLink,
+      this.data,
+      this._paramDefaults
+    );
 
-    return request(parsedUrl, 'PATCH', pick(data, this._modifiableField)).then(data => {
-      return new Result(data, this._url, this.constructor);
-    });
+    return request(parsedUrl, "PATCH", pick(data, this._modifiableField)).then(
+      data => {
+        return new Result(data, this._url, this.constructor);
+      }
+    );
   }
 
   updateLocal(data) {
-    return new this.constructor({...this.data, ...data}, this._url);
+    return new this.constructor({ ...this.data, ...data }, this._url);
   }
 
   /**
-  * Get the required properties for creating a new resource.
-  *
-  * @return array
-  */
+   * Get the required properties for creating a new resource.
+   *
+   * @return array
+   */
   getRequired() {
     return this._required || [];
   }
 
-/**
-  * Validate a new resource.
-  *
-  * @param array data
-  *
-  * @return string{} An object of validation errors.
-  */
+  /**
+   * Validate a new resource.
+   *
+   * @param array data
+   *
+   * @return string{} An object of validation errors.
+   */
   checkNew(values = {}) {
     let errors = {};
     const dataKeys = Object.keys(values);
 
-    const missing = this.getRequired().filter(function(i) {return dataKeys.indexOf(i) < 0;});
+    const missing = this.getRequired().filter(function(i) {
+      return dataKeys.indexOf(i) < 0;
+    });
 
     if (missing.length) {
-      errors._error = `Missing ${missing.join(', ')}`;
+      errors._error = `Missing ${missing.join(", ")}`;
     }
 
-    for(let i = 0 ;i < dataKeys.length;i++) {
+    for (let i = 0; i < dataKeys.length; i++) {
       const key = dataKeys[i];
 
-      errors = { ...errors, ...this.checkProperty(key, values[key])};
+      errors = { ...errors, ...this.checkProperty(key, values[key]) };
     }
 
     return Object.keys(errors).length ? errors : undefined;
   }
 
   save() {
-    if(!this._creatableField.length) {
+    if (!this._creatableField.length) {
       throw new Error("Can't call save on this ressource");
     }
     const errors = this.checkNew(this.data);
 
-    if(errors) {
+    if (errors) {
       return Promise.reject(errors);
     }
     const url = this._queryUrl || this._url;
 
-    return request(url, 'POST', pick(this, this._creatableField)).then(data => {
+    return request(url, "POST", pick(this, this._creatableField)).then(data => {
       return new Result(data, url, this.constructor);
     });
   }
 
   delete() {
-    const deleteLink = this.getLink('#delete');
+    const deleteLink = this.getLink("#delete");
 
-    if(!deleteLink) {
-      throw new Error('Not allowed to delete');
+    if (!deleteLink) {
+      throw new Error("Not allowed to delete");
     }
-    return request(deleteLink, 'DELETE', {}).then(result =>
-      new Result(result, this._url, this.constructor)
+    return request(deleteLink, "DELETE", {}).then(
+      result => new Result(result, this._url, this.constructor)
     );
   }
 
@@ -189,60 +222,68 @@ export default class Ressource {
   }
 
   static wrap(objects) {
-    return objects.map(object => new this.prototype.constructor(object, this._url));
+    return objects.map(
+      object => new this.prototype.constructor(object, this._url)
+    );
   }
 
   /**
-  * Refresh the resource.
-  *
-  */
+   * Refresh the resource.
+   *
+   */
   refresh(params) {
-    return request(this.getUri(), 'GET', params).then(data => {
+    return request(this.getUri(), "GET", params).then(data => {
       this.copy(data);
       return this;
     });
   }
 
   /**
-  * Check whether an operation is available on the resource.
-  *
-  * @param string op
-  *
-  * @return bool
-  */
+   * Check whether an operation is available on the resource.
+   *
+   * @param string op
+   *
+   * @return bool
+   */
   operationAvailable(op) {
     return this.data._links[`#${op}`].href;
   }
 
   /**
-  * Check whether the resource has a link.
-  *
-  * @param rel
-  *
-  * @return bool
-  */
+   * Check whether the resource has a link.
+   *
+   * @param rel
+   *
+   * @return bool
+   */
   hasLink(rel) {
-    return this.data._links && this.data._links[rel] && this.data._links[rel].href;
+    return (
+      this.data._links && this.data._links[rel] && this.data._links[rel].href
+    );
   }
 
   /**
-  * Check whether the resource has an embedded.
-  *
-  * @param rel
-  *
-  * @return bool
-  */
+   * Check whether the resource has an embedded.
+   *
+   * @param rel
+   *
+   * @return bool
+   */
   hasEmbedded(rel) {
-    return this.data._embedded && this.data._embedded[rel] && !!this.data._embedded[rel].length;
+    return (
+      this.data._embedded &&
+      this.data._embedded[rel] &&
+      !!this.data._embedded[rel].length
+    );
   }
 
   /**
-  * Get a embedded for a given resource relation.
-  *
-  * @param string rel
-  *
-  * @return array
-  */
+   * Get a embedded for a given resource relation.
+   *
+   * @param string rel
+   *
+   * @return array
+   */
   getEmbedded(rel) {
     if (!this.hasEmbedded(rel)) {
       throw new Error(`Embedded not found: ${rel}`);
@@ -252,13 +293,13 @@ export default class Ressource {
   }
 
   /**
-  * Get a link for a given resource relation.
-  *
-  * @param string rel
-  * @param bool absolute
-  *
-  * @return string
-  */
+   * Get a link for a given resource relation.
+   *
+   * @param string rel
+   * @param bool absolute
+   *
+   * @return string
+   */
   getLink(rel, absolute = true) {
     if (!this.hasLink(rel)) {
       throw new Error(`Link not found: ${rel}`);
@@ -266,7 +307,7 @@ export default class Ressource {
 
     let _url = this.data._links[rel].href;
 
-    if (absolute && _url.indexOf('//') === -1) {
+    if (absolute && _url.indexOf("//") === -1) {
       _url = this.makeAbsoluteUrl(_url);
     }
 
@@ -274,59 +315,59 @@ export default class Ressource {
   }
 
   /**
-  * Get the resource's URI.
-  *
-  * @param bool absolute
-  *
-  * @return string
-  */
+   * Get the resource's URI.
+   *
+   * @param bool absolute
+   *
+   * @return string
+   */
   getUri(absolute = true) {
-    return this.getLink('self', absolute);
+    return this.getLink("self", absolute);
   }
 
   /**
-  * Make a URL absolute, based on the base URL.
-  *
-  * @param string relativeUrl
-  * @param string _baseUrl
-  *
-  * @return string
-  */
+   * Make a URL absolute, based on the base URL.
+   *
+   * @param string relativeUrl
+   * @param string _baseUrl
+   *
+   * @return string
+   */
   makeAbsoluteUrl(relativeUrl, _baseUrl = this._baseUrl) {
     return `${_baseUrl}${relativeUrl}`;
   }
 
   /**
-  * Execute an operation on the resource.
-  *
-  * @param string op
-  * @param string method
-  * @param array  body
-  *
-  * @return array
-  */
-  runOperation(op, method = 'POST', body) {
+   * Execute an operation on the resource.
+   *
+   * @param string op
+   * @param string method
+   * @param array  body
+   *
+   * @return array
+   */
+  runOperation(op, method = "POST", body) {
     if (!this.operationAvailable(op)) {
       throw new Error(`Oper tion not available: ${op}`);
     }
     return request(this.getLink(`#${op}`), method, body);
   }
   /**
-  * Run a long-running operation.
-  *
-  * @param string op
-  * @param string method
-  * @param array  body
-  *
-  * @return Activity
-  */
-  runLongOperation(op, method = 'POST', body) {
+   * Run a long-running operation.
+   *
+   * @param string op
+   * @param string method
+   * @param array  body
+   *
+   * @return Activity
+   */
+  runLongOperation(op, method = "POST", body) {
     return this.runOperation(op, method, body).then(data => {
       const result = new Result(data, this.getUri());
       const activities = result.getActivities();
 
       if (activities.length !== 1) {
-        throw new Error(`Expected one activity, found ${activities.length }`);
+        throw new Error(`Expected one activity, found ${activities.length}`);
       }
 
       return activities[0];
