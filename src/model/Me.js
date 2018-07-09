e12e154582ea51b47057e3f2bcd3ca54b2d07807
Yann Deshayes <deshayes.yann@gmail.com>
2018-07-09 09:07:52 +0200

    Introduce Me model

---
+++ src/model/Me.js
@@ -0,0 +1,33 @@
+import User from "./User";
+import Ressource from "./Ressource";
+import { getConfig } from "../config";
+
+const url = "/platform/me";
+const paramDefaults = {};
+const modifiableField = ["picture"];
+
+export default class Me extends User {
+  constructor(account) {
+    const { api_url } = getConfig();
+    super(account, `${api_url}${url}`, modifiableField);
+
+    this.projects = [];
+    this.ssh_keys = [];
+    this.organizations = [];
+    this.teams = [];
+    this.picture = "";
+  }
+
+  static get() {
+    const { api_url } = getConfig();
+
+    return super.get({}, `${api_url}${url}`);
+  }
+
+  async update(data) {
+    const { api_url } = getConfig();
+
+    const result = await super.update(data, `${api_url}${url}`);
+    return new Me(result.data); // Account API does not return a Result
+  }
+}
