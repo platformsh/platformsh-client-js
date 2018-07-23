b15a23143d47d0683f69a45c906e6d5f301b128e
Yann Deshayes <deshayes.yann@gmail.com>
2018-07-23 09:14:40 +0200

    Fix sshkey

--- src/model/SshKey.js
+++ src/model/SshKey.js
@@ -1,31 +1,40 @@
-import atob from 'atob';
+import atob from "atob";
 
-import Ressource from './Ressource';
-import { getConfig } from '../config';
+import Ressource from "./Ressource";
+import { getConfig } from "../config";
 
 const paramDefaults = {};
-const url = '/ssh_keys/:id';
+const url = "/platform/ssh_keys/:id";
 
 export default class SshKey extends Ressource {
   constructor(sshKey) {
     const { id } = sshKey;
     const { account_url } = getConfig();
 
-    super(`${account_url}${url}`, paramDefaults, { id }, sshKey, ['title', 'value']);
-    this._required = ['value'];
+    super(`${account_url}${url}`, paramDefaults, { id }, sshKey, [
+      "title",
+      "value"
+    ]);
+    this._required = ["value"];
     this._queryUrl = Ressource.getQueryUrl(`${account_url}${url}`);
-    this.changed = '';
-    this.title = '';
+    this.changed = "";
+    this.id = "";
+    this.title = "";
     this.key_id = 0;
-    this.fingerprint = '';
-    this.value = '';
+    this.fingerprint = "";
+    this.value = "";
   }
 
   static get(params) {
-    const {id, ...queryParams} = params;
+    const { id, ...queryParams } = params;
     const { account_url } = getConfig();
 
-    return super.get(`${account_url}${url}`, { id }, paramDefaults, queryParams);
+    return super.get(
+      `${account_url}${url}`,
+      { id },
+      paramDefaults,
+      queryParams
+    );
   }
 
   /**
@@ -34,8 +43,8 @@ export default class SshKey extends Ressource {
   checkProperty(property, value) {
     const errors = {};
 
-    if(property === 'value' && !this.validatePublicKey(value)) {
-      errors[property] = 'The SSH key is invalid';
+    if (property === "value" && !this.validatePublicKey(value)) {
+      errors[property] = "The SSH key is invalid";
     }
 
     return errors;
@@ -49,22 +58,22 @@ export default class SshKey extends Ressource {
    * @return bool
    */
   validatePublicKey(value) {
-    const filteredValue = value.replace(/\s+/, ' ');
+    const filteredValue = value.replace(/\s+/, " ");
 
-    if (filteredValue.indexOf(' ') === -1) {
+    if (filteredValue.indexOf(" ") === -1) {
       return false;
     }
-    const match = value.split(' ', 3);
+    const match = value.split(" ", 3);
     const type = match[0];
     let key;
 
     try {
       key = atob(match[1]);
-    } catch(err) {
+    } catch (err) {
       return false;
     }
 
-    if (['ssh-rsa', 'ssh-dsa'].indexOf(type) === -1 || !key) {
+    if (["ssh-rsa", "ssh-dsa"].indexOf(type) === -1 || !key) {
       return false;
     }
     return true;
