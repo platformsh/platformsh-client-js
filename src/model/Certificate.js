0e0307de2feebbd77bc872c874658d086f91b63b
Thomas Lattimore <thomas@lattimore.me>
2019-04-25 15:26:43 -0400

    Add 'created_at' to readable property on Cert
    Make the 'created_at' field readable

--- src/model/Certificate.js
+++ src/model/Certificate.js
@@ -1,28 +1,34 @@
-import Ressource from './Ressource';
-import { getConfig } from '../config';
+import Ressource from "./Ressource";
+import { getConfig } from "../config";
 
 const paramDefaults = {};
-const _url = '/projects/:projectId/certificates';
+const _url = "/projects/:projectId/certificates";
 
 export default class Certificate extends Ressource {
   constructor(certificate, url) {
-    super(url, paramDefaults, { }, certificate, ['key', 'certificate', 'chain']);
-    this.key = '';
-    this.id = '';
-    this.certificate = '';
+    super(url, paramDefaults, {}, certificate, ["key", "certificate", "chain"]);
+    this.key = "";
+    this.id = "";
+    this.certificate = "";
     this.chain = [];
     this.domains = [];
-    this.expires_at = '';
-    this.updated_at = '';
+    this.expires_at = "";
+    this.updated_at = "";
+    this.created_at = "";
     this.is_provisioned = true;
     this.issuer = [];
-    this._required = ['key', 'certificate'];
+    this._required = ["key", "certificate"];
   }
 
   static query(params, customUrl) {
     const { projectId, ...queryParams } = params;
     const { api_url } = getConfig();
 
-    return super.query(customUrl || `${api_url}${_url}`, { projectId }, paramDefaults, queryParams);
+    return super.query(
+      customUrl || `${api_url}${_url}`,
+      { projectId },
+      paramDefaults,
+      queryParams
+    );
   }
 }
