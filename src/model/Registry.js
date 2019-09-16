cfd468110507ae0d61c0d95d4c55ae29355ad39b
Thomas Lattimore <thomas@lattimore.me>
2019-09-16 16:44:47 -0400

    Add registry model

---
+++ src/model/Registry.js
@@ -0,0 +1,40 @@
+import Ressource from "./Ressource";
+import { getConfig } from "../config";
+import _urlParser from "../urlParser";
+import request from "../api";
+
+const _url = "/platform/setup/registry";
+const paramDefaults = {};
+// /api/platform/setup/registry\?service\=redis-persistent
+
+export default class Registry extends Ressource {
+  constructor(account, url = `${_url}?service=:name`, modifiableField = []) {
+    const { id } = account;
+
+    super(url, paramDefaults, { id }, account, [], modifiableField);
+    this._queryUrl = Ressource.getQueryUrl(url);
+    this.description = "";
+    this.repo_name = "";
+    this.disk = null;
+    this.docs = {};
+    this.endpoint = "";
+    this.min_disk_size = null;
+    this.name = "";
+    this.runtime = null;
+    this.type = "";
+    this.versions = {};
+  }
+
+  static get(params, customUrl) {
+    const { name, ...queryParams } = params;
+    const { api_url } = getConfig();
+    const url = customUrl || `${api_url}${_url}?service=:name`;
+    const parsedUrl = _urlParser(url, params);
+
+    return request(parsedUrl, "POST", queryParams).then(data => {
+      return typeof data === "undefined"
+        ? undefined
+        : new this.prototype.constructor(data, _url);
+    });
+  }
+}
