2c3547b6fec262ffa05a2f54056d90cd816a3c2e
Thomas Lattimore <thomas@lattimore.me>
2020-03-12 13:55:44 -0400

    Initial commit of AuthProfile
    Initial commit of AuthProfile as just a duplicate of AccountsProfile for
    now.

---
+++ src/model/AuthProfile.js
@@ -0,0 +1,83 @@
+import Ressource from "./Ressource";
+import { getConfig } from "../config";
+
+const url = "/platform/profiles/:id";
+const paramDefaults = {};
+const createableField = [
+  "id",
+  "display_name",
+  "email",
+  "username",
+  "picture",
+  "company_type",
+  "company_role",
+  "company_name",
+  "website_url",
+  "new_ui",
+  "ui_colorscheme",
+  "ui_contrast",
+  "default_catalog",
+  "marketing"
+];
+const modifiableField = [
+  "display_name",
+  "email",
+  "username",
+  "picture",
+  "company_type",
+  "company_role",
+  "company_name",
+  "website_url",
+  "new_ui",
+  "ui_colorscheme",
+  "ui_contrast",
+  "default_catalog",
+  "marketing"
+];
+
+export default class Profile extends Ressource {
+  constructor(profile) {
+    const { api_url } = getConfig();
+    const { id } = profile;
+
+    super(
+      `${api_url}${url}`,
+      paramDefaults,
+      {},
+      profile,
+      createableField,
+      modifiableField
+    );
+    this._queryUrl = Ressource.getQueryUrl(url);
+    this.id = "";
+    this.display_name = "";
+    this.email = "";
+    this.username = "";
+    this.picture = "";
+    this.company_type = "";
+    this.company_role = "";
+    this.company_name = "";
+    this.website_url = "";
+    this.new_ui = "";
+    this.ui_colorscheme = "";
+    this.ui_contrast = "";
+    this.default_catalog = "";
+    this.marketing = "";
+  }
+
+  static get(params, customUrl) {
+    const { id, ...queryParams } = params;
+    const { api_url } = getConfig();
+
+    return super.get(
+      customUrl || `${api_url}${url}`,
+      { id },
+      paramDefaults,
+      queryParams
+    );
+  }
+
+  update(data) {
+    return super.update(data, this.getLink("self"));
+  }
+}
