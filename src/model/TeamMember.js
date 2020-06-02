b552037858c38a018ac77e009d5dfd470b9c022f
MiguelSanchezGonzalez <miguelsanychez@gmail.com>
2020-06-02 12:22:33 +0200

    formats all files with prettier --write "./**/*.js"

--- src/model/TeamMember.js
+++ src/model/TeamMember.js
@@ -1,31 +1,48 @@
-import Ressource from './Ressource';
-import { getConfig } from '../config';
+import Ressource from "./Ressource";
+import { getConfig } from "../config";
 
 const paramDefaults = {};
-const _url = '/platform/teams/:teamId/members';
+const _url = "/platform/teams/:teamId/members";
 
-const creatableField = ['role'];
+const creatableField = ["role"];
 
 export default class TeamMember extends Ressource {
   constructor(teamMember, url) {
     const { teamId } = teamMember;
     const { api_url } = getConfig();
 
-    super(url || `${api_url}${_url}`, paramDefaults, { teamId }, teamMember, creatableField, creatableField);
-    this.user = '';// userId
+    super(
+      url || `${api_url}${_url}`,
+      paramDefaults,
+      { teamId },
+      teamMember,
+      creatableField,
+      creatableField
+    );
+    this.user = ""; // userId
   }
 
   static get(params = {}, customUrl) {
     const { teamId, id, ...queryParams } = params;
     const { api_url } = getConfig();
 
-    return super.get(customUrl || `${api_url}${_url}/:id`, { teamId, id }, paramDefaults, queryParams);
+    return super.get(
+      customUrl || `${api_url}${_url}/:id`,
+      { teamId, id },
+      paramDefaults,
+      queryParams
+    );
   }
 
   static query(params, customUrl) {
     const { teamId, ...queryParams } = params;
     const { api_url } = getConfig();
 
-    return super.query(customUrl || `${api_url}${_url}`, { teamId }, paramDefaults, queryParams);
+    return super.query(
+      customUrl || `${api_url}${_url}`,
+      { teamId },
+      paramDefaults,
+      queryParams
+    );
   }
 }
