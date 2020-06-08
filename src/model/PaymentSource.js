2c303e587d59e6b6a904f7794d59fddb50640b67
shakaman <mbenadon@shakaman.com>
2020-06-08 15:23:12 +0200

    Add payment source model with some methods
     * GET / POST / DELETE: payment source
     * GET: payment sources allowed
     * POST: Setup Intent

---
+++ src/model/PaymentSource.js
@@ -0,0 +1,108 @@
+import Ressource from "./Ressource";
+import { getConfig } from "../config";
+import Result from "./Result";
+import _urlParser from "../urlParser";
+import request from "../api";
+
+const url = "/platform/payment_source";
+const paramDefaults = {};
+const creatableField = ["source_type", "token", "email"];
+
+export default class PaymentSource extends Ressource {
+  constructor(paymentSource) {
+    const { api_url } = getConfig();
+
+    super(`${api_url}${url}`, paramDefaults, {}, paymentSource, creatableField);
+    this.id = "";
+    this.source_type = "";
+    this.name = "";
+    this.number = "";
+    this.card = "";
+    this.mandate = "";
+  }
+
+  static get(queryParams = {}, customUrl) {
+    const { api_url } = getConfig();
+
+    const parsedUrl = _urlParser(
+      customUrl || `${api_url}${url}`,
+      paramDefaults,
+      queryParams
+    );
+
+    return request(parsedUrl, "GET", queryParams)
+      .then(data => {
+        if (typeof data === undefined) return {};
+        // if (data.payment_sources) {
+        //   return data.payment_sources.map(
+        //     ps => new this.prototype.constructor(this.formatDetails(ps))
+        //   );
+        // }
+        return new this.prototype.constructor(
+          this.formatDetails(data.payment_source)
+        );
+      })
+      .catch(err => new this.prototype.constructor());
+  }
+
+  static query(params = {}, customUrl) {
+    const { api_url } = getConfig();
+
+    return super.query(
+      customUrl || `${api_url}${url}`,
+      {},
+      paramDefaults,
+      params
+    );
+  }
+
+  /**
+   * Delete the payment source.
+   *
+   * @return Result
+   */
+  static delete(params) {
+    const { api_url } = getConfig();
+    return request(`${api_url}${url}`, "DELETE", params);
+  }
+
+  /**
+   * Get allowed payment source.
+   * The list of allowed payment sources for the current API consumer.
+   *
+   * @return PaymentSource allowd []
+   */
+  static getAllowed() {
+    const { api_url } = getConfig();
+    return request(`${api_url}${url}/allowed`, "GET");
+  }
+
+  /**
+   * Create a Setup Intent.
+   *
+   * @return SetupIntent
+   */
+  static intent() {
+    const { api_url } = getConfig();
+    return request(`${api_url}${url}/intent`, "POST");
+  }
+
+  /**
+   * Format data according to the type of payment source
+   *
+   * @param paymentSource PaymentSource
+   *
+   * @return PaymentSource
+   */
+  static formatDetails(paymentSource) {
+    switch (paymentSource.source_type) {
+      case "credit-card":
+        paymentSource.card = paymentSource.data;
+        break;
+      case "stripe_sepa_debit":
+        paymentSource.mandate = paymentSource.data;
+        break;
+    }
+    return paymentSource;
+  }
+}
