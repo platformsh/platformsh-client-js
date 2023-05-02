ec45d886fcd696c19699b5723a4057a958f9fbac
Paulina <paulina.gumienna@platform.sh>
2023-05-02 07:59:45 +0200

    update getActivities args

--- src/model/Environment.ts
+++ src/model/Environment.ts
@@ -346,8 +346,9 @@ export default class Environment extends Ressource {
    *
    * @return Activity[]
    */
-  async getActivities(type: string, starts_at: number) {
-    const params = { type, starts_at };
+  async getActivities(type: string, starts_at?: Date) {
+    const startsAt = starts_at?.toISOString();
+    const params = { type, starts_at: startsAt };
 
     return Activity.query(params, `${this.getUri()}/activities`);
   }
